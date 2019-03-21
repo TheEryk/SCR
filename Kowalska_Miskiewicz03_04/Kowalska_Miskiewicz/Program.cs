f14d41598a69c78b1084933596c4c6b2e26a6114
Eryk Miskiewicz <erykmisiek@gmail.com>
2019-03-21 19:23:38 +0100

    1

---
+++ Kowalska_Miskiewicz03_04/Kowalska_Miskiewicz/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace lab2
+{
+    class Program
+    {
+        interface IRunnable
+        {
+            void Run();
+            bool HasFinished
+            {
+                get; set;
+            }
+        }
+        abstract class Agent : IRunnable
+        {
+            public int ID;
+            public bool HasFinished
+            {
+                get;
+                set;
+            }
+            public Agent(int iD)
+            {
+                ID = iD;
+                HasFinished = false;
+            }
+
+            public abstract void Update();
+            public void Run()
+            {
+                while (!HasFinished)
+                {
+                    Update();
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        class ConstantCountingAgent : Agent
+        {
+            int count;
+            public ConstantCountingAgent(int iD): base(iD)
+            {
+            }
+            public override void Update()
+            {
+                if (count < 10)
+                {
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine(ID);
+                    HasFinished = true;
+                }
+            }
+        }
+        
+        class CountingAgent : Agent
+        {
+            int count;
+            public CountingAgent(int iD): base(iD)
+            {
+            }
+            public override void Update()
+            {
+                if (count < ID)
+                {
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine(ID);
+                    HasFinished = true;
+                }
+            }
+        }
+        
+        class SineGeneratingAgent : Agent
+        {
+            int count;
+            public double output;
+            public SineGeneratingAgent(int iD): base(iD)
+            {
+                count = 0;
+                output = 0;
+            }
+            int SinGen()
+            {
+                return 0;
+            }
+            public override void Update()
+            {
+               if(count < 10*(ID % 10))
+                {
+                    output = Math.Sin(count);
+                    Console.WriteLine(output);
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine(ID);
+                    HasFinished = true;
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            SineGeneratingAgent test = new SineGeneratingAgent(15);
+            
+            test.Run();
+            
+            Console.Read();
+        }
+        
+        
+    }
+
+    
+}
---
+++ Kowalska_Miskiewicz03_18/03_18/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_18
+{
+    class Program
+    {
+        static List<int> lista = new List<int>();
+        static Random random = new Random();
+        static List<Agent> agenty = new List<Agent>();
+        stat
+        static void GenerateList()
+        {
+
+            for (int j = 0; j < 1000; j++)
+            {
+                lista.Add(random.Next(0, 100));
+            }
+        }
+        static void GenerateRunnables()
+        {
+            int i = 0;
+            bool divided = false;
+            do
+            {
+                if (i < (lista.Count - 1))
+                {
+                    agenty.Add(new CountingAgent(lista.GetRange(i, i + 249)));
+                }
+                else
+                {
+                    agenty.Add(new CountingAgent(lista.GetRange(i, lista.Count - 1)));
+                    divided = true;
+                }
+                i += 250;
+
+            } while (!divided);
+        }
+        static void RunRunnables()
+        {
+            foreach(var agent in agenty)
+            {
+                agent.Run();
+            }
+
+        }
+        abstract class Agent
+        {
+            public bool HasFinished;
+            public abstract void Run();
+        }
+        class CountingAgent : Agent
+        {
+            public int wynik;
+            List<int> liczby;
+            List<int> liczbyOut;
+            public CountingAgent(List<int> Liczby, List<int> ListaOut)
+            {
+                liczby = Liczby;
+                HasFinished = false;
+                wynik = 0;
+                liczbyOut = ListaOut;
+            }
+            public override void Run()
+            {
+                int temp = 0;
+                foreach(var i in liczby)
+                {
+                    temp += i;
+                }
+                liczbyOut.Add(temp);
+                HasFinished = true;
+                
+            }
+        }
+
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
