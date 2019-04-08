574e1da1de3e7b382efc7678a9d643429adb1c76
TheEryk <erykmisiek@gmail.com>
2019-04-08 18:19:07 +0200

    04_08

--- Kowalska_Miskiewicz04_01/Kowalska_Miskiewicz04_01/Program.cs
+++ Kowalska_Miskiewicz04_01/Kowalska_Miskiewicz04_01/Program.cs
@@ -9,7 +9,10 @@ namespace Kowalska_Miskiewicz04_01
 {
     class Program
     {
-
+        private static Mutex mut = new Mutex();
+        static private readonly object x = new object();
+        private static SpinLock sl = new SpinLock();
+        private static bool lockTaken = false;
         abstract class Agent
         {
             public int ID;
@@ -26,6 +29,7 @@ namespace Kowalska_Miskiewicz04_01
                 while (!HasFinished)
                 {
                     Update();
+                    Thread.Sleep(1000);
                 }
             }
         }
@@ -44,7 +48,7 @@ namespace Kowalska_Miskiewicz04_01
             public override void Update()
             {
                 Console.WriteLine("Stan konta: " + stanKonta);
-                Thread.Sleep(2000);
+                Thread.Sleep(1000);
             }
             
 
@@ -60,35 +64,79 @@ namespace Kowalska_Miskiewicz04_01
         
         class KasjerAd : Kasjer
         {
+            int JD;
             int tempStanKonta;
             Bank tempBank;
             public KasjerAd(int iD, Bank bank) : base(iD, bank)
             {
                 tempBank = bank;
+                JD = iD;
             }
             public override void Update()
             {
+                /*
+                mut.WaitOne();
                 tempStanKonta = tempBank.stanKonta;
-                tempStanKonta += 100;
-                Thread.Sleep(100);
+                tempStanKonta += 200;
                 tempBank.setStanKonta(tempStanKonta);
+                Console.WriteLine("Dodałem 200, moj ID:" + JD + " stan banku:" + tempBank.stanKonta);
+                mut.ReleaseMutex();
+                */
+                /*
+                lock (x)
+                {
+                    tempStanKonta = tempBank.stanKonta;
+                    tempStanKonta += 200;
+                    tempBank.setStanKonta(tempStanKonta);
+                    Console.WriteLine("Dodałem 200, moj ID:" + JD + " stan banku:" + tempBank.stanKonta);
+                }
+                */
+
+                lockTaken = false;
+                try
+                    {
+                        sl.Enter(ref lockTaken);
+                        //lockTaken = true;
+                    }
+                    finally
+                    {
+                        tempStanKonta = tempBank.stanKonta;
+                        tempStanKonta += 200;
+                        tempBank.setStanKonta(tempStanKonta);
+                        Console.WriteLine("Dodałem 200, moj ID:" + JD + " stan banku:" + tempBank.stanKonta);
+                        if (lockTaken) sl.Exit();
+                    }
+                
             }
         }
 
         class KasjerSub : Kasjer
         {
+            int JD;
             int tempStanKonta;
             Bank tempBank;
             public KasjerSub(int iD, Bank bank) : base(iD, bank)
             {
                 tempBank = bank;
+                JD = iD;
             }
             public override void Update()
             {
+                /*
+                mut.WaitOne();
                 tempStanKonta = tempBank.stanKonta;
-                tempStanKonta -= 100;
-                Thread.Sleep(100);
+                tempStanKonta -= (100);
                 tempBank.setStanKonta(tempStanKonta);
+                mut.ReleaseMutex();
+                Console.WriteLine("Zabrałem 100, moj ID:" + JD + " stan banku:" + tempBank.stanKonta);
+                */
+                lock (x)
+                {
+                    tempStanKonta = tempBank.stanKonta;
+                    tempStanKonta -= 100;
+                    tempBank.setStanKonta(tempStanKonta);
+                    Console.WriteLine("Zabrałem 100, moj ID:" + JD + " stan banku:" + tempBank.stanKonta);
+                }
             }
         }
 
@@ -97,7 +145,7 @@ namespace Kowalska_Miskiewicz04_01
         {
             Bank bank = new Bank(1);
             List<Kasjer> kasjerzy = new List<Kasjer>();
-            for(int i=0; i<10; i++)
+            for(int i=0; i<6; i++)
             {
                 if (i % 2 == 0)
                 {
@@ -111,9 +159,14 @@ namespace Kowalska_Miskiewicz04_01
             List<Thread> watki = new List<Thread>();
             foreach (var kasjer in kasjerzy)
             {
-                watki.Add(new Thread(kasjer.Run));
+                Thread thread = new Thread(kasjer.Run);
+                watki.Add(thread);
+                thread.Start();
+                
             }
-            bank.Run();
+            
+            Thread thread2 = new Thread(bank.Run);
+            thread2.Start();
             //Thread thread1 = new Thread(kasa1.Run);
             //bank.Run();
         }
